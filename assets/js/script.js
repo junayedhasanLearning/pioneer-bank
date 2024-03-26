a2a350cc86781cfe42e6373d7d96daa93c42d7eb
junayedhasanLearning <junayedhasan0271@gmail.com>
2024-03-26 22:42:27 +0600

    Full Functioning withdraw and deposit system

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,26 +9,42 @@ loginBtn.addEventListener('click', () => {
 });
 
 
-// Deposit Button Event Handler
-
 
 function updateSpan(id, amount) {
     let amountNumber = document.getElementById(id).innerText;
     amountNumber = parseFloat(amountNumber);
     const total = amount + amountNumber;
     document.getElementById(id).innerText = total;
-}
+};
 
+function getInputValue(id){
+    let inputValue = document.getElementById(id).value;
+    inputValue = parseFloat(inputValue);
+    return inputValue;
+};
 
+// Deposit Button Event Handler
 const depositBtn = document.getElementById('depositBtn');
 depositBtn.addEventListener('click', () => {
     // deposit input field declare
-    let depositAmount = document.getElementById('depositAmount').value;
-    depositAmount = parseFloat(depositAmount);
+    let depositAmount = getInputValue('depositAmount');
     // deposit money 
     updateSpan("depositMoney", depositAmount)
     // balance amount 
     updateSpan("balanceMoney", depositAmount);
     document.getElementById('depositAmount').value = "";
 
+});
+
+
+// Withdraw Button Event Handler
+
+const withdrawBtn = document.getElementById('addWithdraw');
+withdrawBtn.addEventListener('click', () => {
+    const withdrawAmount = getInputValue('withdrawAmountInput');
+
+    updateSpan("totalWithdrawAmount", withdrawAmount);
+    updateSpan("balanceMoney", (-1 * withdrawAmount));
+
+    document.getElementById('withdrawAmountInput').value = "";
 });
